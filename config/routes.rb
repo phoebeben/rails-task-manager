ce584acb920a6aa6a613935fa561a5011c96ec3b
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-05-23 12:45:38 +0100

    added links

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get '/tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/:id', to: 'tasks#show', as: :show
 end
